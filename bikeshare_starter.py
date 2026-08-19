67f743ee889a59e60985932f359d9f9e81ecadde
Dimosthenis Gatopoulos <gatopdimos@gmail.com>
2026-08-19 20:12:18 +0300

    refactor: Improve user input filters

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -6,6 +6,10 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+MONTH_DATA = {"January":1, "February":2, "March":3, "April":4, "May":5, "June":6, "July":7, "August":8, "September":9, "October":10, "November":11, "December":12}
+
+WEEKDAY_DATA = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -16,15 +20,47 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    
+    while True:
+        city_user_input = input("From the defined list of cities (Chicago, New York City, Washington) type the name of the one you wish to analyze: ")
+        # remove white spaces and change the user input value to lowercase
+        city_lower_case = city_user_input.strip().lower()
+        if city_lower_case in CITY_DATA:
+            city = city_lower_case
+            break
+        else:
+            print("Warning: The city provided does not exist in our dataset. In the follow up request, please provide a city name that exists in our dataset such as Chicago, New York City or Washington.\n")
 
+    # TO DO: get user input for month (all, january, february, ... , june)
+    
+    while True:
+        month_user_input = input("Please type the name of the month for which you wish to view data for (e.g. May) if you explicitely want to see data of a specific month. Alternatively, type 'All' to have all months in the end result:\n")
+        # remove white spaces and change the user input value to lowercase
+        month_title = month_user_input.strip().title()
+        if month_title in MONTH_DATA:
+            month = month_title
+            break
+        elif month_title == "All":
+            month = "all"
+            break
+        else:
+            print("Warning: The value provided for month is not correct. Please type the name of the month for which you wish to view data for (e.g. May) or type 'All' to have all months in the end result.\n")
 
-    # get user input for month (all, january, february, ... , june)
+    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
 
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-
-
+    while True:
+        week_day_user_input = input("Please type the name of the day of the week for which you wish to view data for (e.g. Monday) if you explicitely want to see data of a specifc day of the week. Alternatively, type 'All' to have all days of the week in the end result:\n")
+        week_day_title = week_day_user_input.strip().title()
+        if week_day_title in WEEKDAY_DATA:
+            day = week_day_title
+            break
+        elif week_day_title == "All":
+            day = "all"
+            break
+        else:
+            print("Warning: The value provided for the day of the week is not correct. Please type the name of the day of the week for which you wish to view data for (e.g. Monday).Alternatively, type 'All' to have all days of the week in the end result.\n")
     print('-'*40)
     return city, month, day
 
